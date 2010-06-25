3fc47a2704e113999673b983ce622e9320c25715
niklas <niklas@brueckenschlaeger.de>
2010-06-25 03:35:30 +0200

    only sending message, not whole error spec

--- lib/casserver/controllers.rb
+++ lib/casserver/controllers.rb
@@ -206,7 +206,7 @@ module CASServer::Controllers
 
         # ECHO specific:
         #   redirecting back, with the error message
-        return redirect(service_uri_with_message(@service, @message.to_json))
+        return redirect(service_uri_with_message(@service, @message[:message]))
       end
 
       render :login
